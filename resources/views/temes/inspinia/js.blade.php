b126bc594b4bcb2bc114681b7378830d2e315b9b
Oscar Acuña Williams <oacuna@innove.co.cr>
2019-05-03 21:06:33 -0600

    Cambios

---
+++ resources/views/temes/inspinia/js.blade.php
@@ -0,0 +1,10 @@
+    <!-- Mainly scripts -->
+    <script src="{!! asset('assets/inspinia/js/jquery-3.1.1.min.js') !!}"></script>
+    <script src="{!! asset('assets/inspinia/js/popper.min.js') !!}"></script>
+    <script src="{!! asset('assets/inspinia/js/bootstrap.js') !!}"></script>
+    <script src="{!! asset('assets/inspinia/js/plugins/metisMenu/jquery.metisMenu.js') !!}"></script>
+    <script src="{!! asset('assets/inspinia/js/plugins/slimscroll/jquery.slimscroll.min.js') !!}"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{!! asset('assets/inspinia/js/inspinia.js') !!}"></script>
+    <script src="{!! asset('assets/inspinia/js/plugins/pace/pace.min.js') !!}"></script>
