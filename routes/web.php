f77275abda3ed344a876a29944382320fe5098fa
JoseDavidQuesada <josedavidquesada2017@gmail.com>
2019-05-08 19:23:15 -0600

    Commit 20190508 Login validando con user con plantilla

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
-=======
 Route::get('/home', function () {
     return view('temes.inspinia.test.test');
 });
->>>>>>> b126bc594b4bcb2bc114681b7378830d2e315b9b
