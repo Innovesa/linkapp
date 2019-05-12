18816f2782f14654e4eaceb718c1f9e80735e05d
unknown <josedavidquesada2017@gmail.com>
2019-05-12 00:44:09 -0600

    Commit 20190511 con internacionalizacion

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -51,11 +51,11 @@ class MailResetPasswordNotification extends Notification
         }
 
         return (new MailMessage)
-            ->subject(Lang::getFromJson('Reset Password Notification'))
-            ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', ['token' => $this->token], false)))
-            ->line(Lang::getFromJson('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.users.expire')]))
-            ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
+            ->subject(Lang::getFromJson(__('auth.Reset Password Notification')))
+            ->line(Lang::getFromJson(_('auth.You are receiving this email because we received a password reset request for your account')))
+            ->action(Lang::getFromJson('auth.Reset Password'), url(config('app.url').route('password.reset', ['token' => $this->token], false)))
+            ->line(Lang::getFromJson(_('auth.This password reset link will expire in :count minutes'), ['count' => config('auth.passwords.users.expire')]))
+            ->line(Lang::getFromJson(_('auth.If you did not request a password reset, no further action is required')));
     }
 
     /**
--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,29 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-    'hola' => 'Empanada'
+    'hola' => 'Empanada',
+    'Login' => 'Login',
+    'Logout' =>'Logout',
+    'Register' => 'Register',
+    'E-MailAddress/Username'=> 'E-Mail Address/Username',
+    'Username' => 'Username',
+    'E-Mail Address' => 'E-Mail Address',
+    'Confirm Password' => 'Confirm Password',
+    'Password' => 'Password',
+    'Reset Password' => 'Reset Password',
+    'Send Password Reset Link' => 'Send Password Reset Link',
+    'We have e-mailed your password reset link' => 'We have e-mailed your password reset link!',
+    'Remember Me' => 'Remember Me',
+    'Forgot Your Password' => 'Forgot Your Password?',
+    'Verify Your Email Address' => 'Verify Your Email Address',
+    'Before proceeding, please check your email for a verification link'=> 'Before proceeding, please check your email for a verification link.',
+    'If you did not receive the email' => 'If you did not receive the email',
+    'click here' => 'click here',
+    'A fresh verification link has been sent to your email address' => 'A fresh verification link has been sent to your email address.',
+
+    /**Correos */
+    'Reset Password Notification' => 'Reset Password Notification',
+    'You are receiving this email because we received a password reset request for your account'=>'You are receiving this email because we received a password reset request for your account.',
+    'This password reset link will expire in :count minutes' =>'This password reset link will expire in :count minutes.',
+    'If you did not request a password reset, no further action is required' => 'If you did not request a password reset, no further action is required.'
 ];
